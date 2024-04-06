3c4909f4556d98e5036c84b81bfdd5be25b6731b
miyama <kaa74395@gmail.com>
2024-04-06 15:59:30 +0900

    adjustment-new-song

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#btn-menu");
+const nav = document.querySelector("#nav");
+console.log("12424");
+
+btn.addEventListener("click", () => {
+  nav.classList.add("open-menu");
+});
